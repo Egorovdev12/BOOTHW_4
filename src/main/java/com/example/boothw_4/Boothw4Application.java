affe27bf6e4d8fea6020dbece4372dff17761ba5
ILYA EGOROV <egorovcf@gmail.com>
2022-02-26 04:51:19 +0500

    Initial commit

---
+++ src/main/java/com/example/boothw_4/Boothw4Application.java
@@ -0,0 +1,13 @@
+package com.example.boothw_4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Boothw4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Boothw4Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/boothw_4/Boothw4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.boothw_4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Boothw4ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
